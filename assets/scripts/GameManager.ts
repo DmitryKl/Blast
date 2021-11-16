a85942cee0fe8de6d699888ea718f061d9e3535e
DmitryKl <dmitry7152@gmail.com>
2021-11-16 15:40:41 +0500

    Рефакторинг для удобного масштабирования

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, SpriteFrame, Sprite, Prefab } from 'cc';
+import { _decorator, Component, Node, SpriteFrame, Sprite, Prefab, Vec2, Size } from 'cc';
 import * as modules from 'cc';
 import { BlastGame } from './non-components/BlastGame';
 import { Position, Utils } from "./non-components/Utils";
@@ -21,11 +21,13 @@ export class GameManager extends Component {
     maxReshuffleCount = 5;
     @property
     minCombinationCount = 2;
+    @property
+    fieldPaddingY = 10;
+    @property
+    fieldPaddingX = 10;
 
-    @property({ type: modules.SpriteAtlas })
-    fieldAtlas: modules.SpriteAtlas = null;
-    @property({ type: [Prefab], tooltip: "0 - Corner, 1 - horizontal side, 2 - vertical side, 3 - inner" })
-    fieldCellPrefabs: Prefab[] = [];
+    @property({ type: Node })
+    field: Node = null;
 
     @property({ type: [SpriteFrame] })
     tileSpriteFrames: SpriteFrame[] = [];
@@ -33,14 +35,12 @@ export class GameManager extends Component {
     tilePrefab: Prefab = null;
 
     @property({ type: Prefab })
-    cupColliderPrefab: Prefab = null;
-
-    @property({type: Prefab})
     removePSPrefab: Prefab = null;
 
     tiles: Tile[][];
 
-    tileSize: number;
+    tileSize: Size;
+    tileStartPoint: Vec2;
 
     game: BlastGame;
 
@@ -52,9 +52,17 @@ export class GameManager extends Component {
             this.maxReshuffleCount,
             this.minCombinationCount);
 
-        this.game.removeTileEvent = (tilePosition: Position) => { 
-            let removePS = modules.instantiate(this.removePSPrefab);            
-            this.node.addChild(removePS);
+
+        this.bindEvents();
+        this.calculateSizes();
+        this.initTiles();
+        this.fillTileNodes();
+    }
+
+    private bindEvents() {
+        this.game.removeTileEvent = (tilePosition: Position) => {
+            let removePS = modules.instantiate(this.removePSPrefab);
+            this.field.addChild(removePS);
             removePS.setPosition(this.tiles[tilePosition.row][tilePosition.column].node.position);
 
             this.tiles[tilePosition.row][tilePosition.column].node.destroy();
@@ -68,65 +76,35 @@ export class GameManager extends Component {
         };
 
         this.game.createTileEvent = (tilePosition: Position, tileType: number) => {
-            this.tiles[tilePosition.row][tilePosition.column].type = tileType; 
+            this.tiles[tilePosition.row][tilePosition.column].type = tileType;
         };
 
         this.game.stateChangedEvent = this.fillTileNodes.bind(this);
-        
-        this.tileSize = (<Node>this.tilePrefab.data).getComponent(modules.UITransform).contentSize.width;
-
-        this.createCup();
-        this.createField();
-        this.initTiles();
-        this.fillTileNodes();
     }
 
-    private createField() {
-        let startY = this.node.position.y - this.tileSize * this.height / 2;
-        let startX = this.node.position.x - this.tileSize * this.width / 2;
-        let offsetY = ((<Node>this.fieldCellPrefabs[0].data).getComponent(modules.UITransform).contentSize.height - this.tileSize) / 2;
-        let offsetX = ((<Node>this.fieldCellPrefabs[0].data).getComponent(modules.UITransform).contentSize.width - this.tileSize) / 2;
+    private calculateSizes() {
+        this.tileSize = new Size(
+            (this.field.getComponent(modules.UITransform).width - 2 * this.fieldPaddingX) / this.width,
+            (this.field.getComponent(modules.UITransform).height - 2 * this.fieldPaddingY) / this.height
+        );
 
-        // bottom 
-        this.createNodeWithSprite(this.fieldCellPrefabs[0], this.fieldAtlas.getSpriteFrame("field7"), startX - offsetX, startY - offsetY);
-        for (let i = 1; i < this.width - 1; i++) {
-            this.createNodeWithSprite(this.fieldCellPrefabs[1], this.fieldAtlas.getSpriteFrame("field8"), startX + this.tileSize * i, startY - offsetY);
-        }
-        this.createNodeWithSprite(this.fieldCellPrefabs[0], this.fieldAtlas.getSpriteFrame("field9"), startX + this.tileSize * (this.width - 1) + offsetX, startY - offsetY);
+        this.tileStartPoint = new Vec2(
+            - this.tileSize.width * (this.width - 1) / 2,
+            this.tileSize.height * this.height / 2
+        );
 
-        // middle
-        for (let row = 1; row < this.height - 1; row++) {
-            this.createNodeWithSprite(this.fieldCellPrefabs[2], this.fieldAtlas.getSpriteFrame("field4"), startX - offsetX, startY + this.tileSize * row);
-            for (let i = 1; i < this.width - 1; i++) {
-                this.createNodeWithSprite(this.fieldCellPrefabs[3], this.fieldAtlas.getSpriteFrame("field5"), startX + this.tileSize * i, startY + this.tileSize * row);
-            }
-            this.createNodeWithSprite(this.fieldCellPrefabs[2], this.fieldAtlas.getSpriteFrame("field6"), startX + this.tileSize * (this.width - 1) + offsetX, startY + this.tileSize * row);
-        }
-
-        // top
-        this.createNodeWithSprite(this.fieldCellPrefabs[0], this.fieldAtlas.getSpriteFrame("field1"), startX - offsetX, startY + this.tileSize * (this.height - 1) + offsetY);
-        for (let i = 1; i < this.width - 1; i++) {
-            this.createNodeWithSprite(this.fieldCellPrefabs[1], this.fieldAtlas.getSpriteFrame("field2"), startX + this.tileSize * i, startY + this.tileSize * (this.height - 1) + offsetY);
-        }
-        this.createNodeWithSprite(this.fieldCellPrefabs[0], this.fieldAtlas.getSpriteFrame("field3"), startX + this.tileSize * (this.width - 1) + offsetX, startY + this.tileSize * (this.height - 1) + offsetY);
-
-    }
-
-    private createNodeWithSprite(prefab: Prefab, spriteFrame: SpriteFrame, x: number, y: number): Node {
-        let newNode: Node = modules.instantiate(prefab);
-        this.node.addChild(newNode);
-        newNode.setPosition(x, y);
-        let spriteComponent: modules.Sprite = newNode.getComponent(Sprite);
-        spriteComponent.spriteFrame = spriteFrame;
-
-        return newNode;
-    }
-
-    private createCup() {
-        let newNode = modules.instantiate(this.cupColliderPrefab);
-        this.node.addChild(newNode);
-        newNode.getComponent(modules.BoxCollider2D).size.width = this.width * this.tileSize;
-        newNode.setPosition(this.node.position.x, this.node.position.y - (this.height + 1) * this.tileSize / 2);
+        this.tilePrefab.data.getComponent(modules.UITransform).contentSize = new Size(
+            this.tileSize.width,
+            this.tileSize.height * 1.1
+        );
+        this.tilePrefab.data.getComponent(modules.BoxCollider2D).size = new Size(
+            this.tileSize.width - 2,
+            this.tileSize.height
+        );
+        this.tilePrefab.data.getComponent(modules.BoxCollider2D).offset = new Vec2(
+            0,
+            -this.tileSize.height * 0.04
+        );
     }
 
     private initTiles() {
@@ -134,41 +112,39 @@ export class GameManager extends Component {
         for (let row = 0; row < this.height; row++) {
             this.tiles[row] = new Array<Tile>(this.width);
             for (let column = 0; column < this.width; column++) {
-                this.tiles[row][column] = new Tile(new Position(row, column), this.game.tiles[row][column]);                
+                this.tiles[row][column] = new Tile(new Position(row, column), this.game.tiles[row][column]);
             }
         }
     }
 
     private fillTileNodes() {
-        let startY: number = this.node.position.y + this.tileSize * this.height / 2;
-        let startX: number = this.node.position.x - this.tileSize * this.width / 2;        
-
         let offsetsY = Utils.initArray(this.width, 0);
 
         for (let row = 0; row < this.height; row++) {
             for (let column = 0; column < this.width; column++) {
-                if(this.tiles[row][column].node == null) {                    
+                if (this.tiles[row][column].node == null) {
                     this.tiles[row][column].node = this.createTileNode(
-                        this.tiles[row][column], 
-                        startX + column * this.tileSize,
-                        startY + offsetsY[column]);
-                    offsetsY[column] += this.tileSize;
-                }              
+                        this.tiles[row][column],
+                        new Vec2(
+                            this.tileStartPoint.x + column * this.tileSize.width,
+                            this.tileStartPoint.y + offsetsY[column]
+                        )
+                    );
+                    offsetsY[column] += this.tileSize.height;
+                }
             }
         }
-
-                    
     }
 
-    private createTileNode(tile: Tile, x: number, y: number): Node {
-        let node = this.createNodeWithSprite(this.tilePrefab, this.tileSpriteFrames[tile.type], x, y);
-        // node.on(Node.EventType.MOUSE_DOWN, function(event) {
-        //     this.game.tapTile(this.getComponent(TileComponent).tile.position);
-        //     this.fillTileNodes();
-        // }.bind(node));
-        node.getComponent(TileComponent).tile = tile;
-        node.getComponent(TileComponent).game = this.game;
-        
-        return node;
+    private createTileNode(tile: Tile, position: Vec2): Node {
+        let newNode: Node = modules.instantiate(this.tilePrefab);     
+        this.field.addChild(newNode);
+        newNode.setPosition(position.x, position.y);
+        newNode.getComponent(Sprite).spriteFrame = this.tileSpriteFrames[this.tiles[tile.position.row][tile.position.column].type];        
+                
+        newNode.getComponent(TileComponent).tile = tile;
+        newNode.getComponent(TileComponent).game = this.game;
+
+        return newNode;
     }
 }
